e779ac15aaf9361e4ab70cca0d3735da7b6853d3
pecca86 <pekka.rantaaho86@gmail.com>
2024-09-01 12:12:32 +0200

    - Stats for all accounts works.

--- src/main/java/com/pekka/moni/statistics/StatisticsService.java
+++ src/main/java/com/pekka/moni/statistics/StatisticsService.java
@@ -1,12 +1,10 @@
 package com.pekka.moni.statistics;
 
 import com.pekka.moni.account.Account;
-import com.pekka.moni.account.AccountRepository;
 import com.pekka.moni.auth.LoggedInCustomerService;
 import com.pekka.moni.customer.Customer;
 import com.pekka.moni.statistics.dto.AccountWithTransactions;
 import com.pekka.moni.statistics.dto.StatisticsAllAccountsResponse;
-import com.pekka.moni.statistics.dto.TransactionSumsPerMonth;
 import com.pekka.moni.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.Year;
 import java.util.*;
 
 @Service
@@ -34,42 +31,28 @@ public class StatisticsService {
 
         List<Account> customerAccounts = loggedInCustomer.getAccounts();
         List<AccountWithTransactions> results = new ArrayList<>();
-        Map<Month, BigDecimal> resultForMonthMap = constructMonthMap();
-        LocalDate startDate = LocalDate.now();
+        Map<Month, BigDecimal> resultForMonthMap;
+        LocalDate startDate;
 
         for (Account account : customerAccounts) {
             resultForMonthMap = constructMonthMap();
             startDate = LocalDate.now();
-            System.out.println("RESETTED STARTDATE TO: " + startDate);
             List<Transaction> transactions = account.getTransactions();
             for (Transaction transaction : transactions) {
-                System.out.println("TRANSACTION DATE IS: " + transaction.getTransactionDate());
                 if (transactionIsWithinThisYearSpan(transaction)) {
-                    System.out.println("CALCUTAING FOR TRANSACTION: " + transaction.getSum() + " WITH TARGET DATE: " + startDate);
-//                    if (transactionBelongsToTargetMonth(transaction, Month.from(startDate))) {
                         resultForMonthMap.computeIfPresent(Month.from(transaction.getTransactionDate()), (k, currentMonthSum) -> currentMonthSum.add(transaction.getSum()));
-//                    }
                 }
                 startDate = startDate.plusMonths(1);
             }
             results.add(new AccountWithTransactions(account, resultForMonthMap));
         }
 
-
-
-
-//        System.out.println("NEW VALUES FOR MAP ARE: " + resultForMonthMap);
         return ResponseEntity.ok(new StatisticsAllAccountsResponse(results));
     }
 
     private boolean transactionIsWithinThisYearSpan(Transaction transaction) {
-//        return transaction.getTransactionDate().getYear() <= year.getValue() && transaction.getTransactionDate().getYear() >= year.getValue() - 1;
         LocalDate startDate = LocalDate.now();
         return transaction.getTransactionDate().isBefore(startDate.plusYears(1)) && transaction.getTransactionDate().isAfter(startDate.minusDays(1));
-    }
-
-    private boolean transactionBelongsToTargetMonth(Transaction transaction, Month month) {
-        return transaction.getTransactionDate().getMonth().equals(month);
     }
 
     private Map<Month, BigDecimal> constructMonthMap() {
