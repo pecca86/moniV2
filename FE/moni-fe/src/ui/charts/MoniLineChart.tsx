c883058898f20141dd111db5690b1fba499956db
pecca86 <pekka.rantaaho86@gmail.com>
2024-09-01 21:23:10 +0200

    - added functionality to show / hide chart line by clicking on the legend

--- FE/moni-fe/src/ui/charts/MoniLineChart.tsx
+++ FE/moni-fe/src/ui/charts/MoniLineChart.tsx
@@ -2,6 +2,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 import MoniToolTip from '../cta/MoniToolTip';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import { AccountsStatisticData } from '../../types/global';
+import { useState } from 'react';
 
 interface Props {
     accountStatisticsData: AccountsStatisticData
@@ -12,7 +13,7 @@ interface LineChartData {
     name?: string | undefined;
 }
 
-const MoniLineChart = ({accountStatisticsData}: Props) => {
+const MoniLineChart = ({ accountStatisticsData }: Props) => {
     const newData: LineChartData[] = []
 
     // Create an initial data point, cointaing just the months in the same order we receive them from the server
@@ -43,30 +44,46 @@ const MoniLineChart = ({accountStatisticsData}: Props) => {
         return hash;
     }
 
+    const [hiddenNameList, setHiddenNameList] = useState([]);
+
+    function pippeliPushed(e) {
+        console.log(e);
+        let legend: string = e.dataKey
+        if (hiddenNameList.includes(legend)) {
+            setHiddenNameList((hiddenNameList) => hiddenNameList.filter(item => item !== e.dataKey));
+        } else {
+            setHiddenNameList([...hiddenNameList, e.dataKey]);
+        }
+    }
+
     return (
         <div className='bg-white rounded-lg p-1 shadow-md'>
+            {/* <button onClick={() => showHide('Account 3')}>Show / Hide</button> */}
             <ResponsiveContainer width="100%" height={300}>
                 <LineChart width={500} height={300} data={newData}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
                     <YAxis />
                     <Tooltip />
-                    <Legend />
+                    <Legend onClick={(e) => pippeliPushed(e)} />
                     {accountStatisticsData.data.map(dp => {
                         // create a unique color for each account line
-                        const randomHexColor = Math.floor(stringToHash(dp.account.iban) * 1888438).toString(16).substring(0,6);
+                        const randomHexColor = Math.floor(stringToHash(dp.account.iban) * 1888438).toString(16).substring(0, 6);
                         return (
                             <Line
+                                id='KUUK'
                                 type="monotone"
                                 dataKey={dp.account.name.trim()}
                                 stroke={`#${randomHexColor}`}  //"#8884d8"
                                 activeDot={{ r: 8 }}
+                                // hide={dp.account.name.trim() === hiddenName}
+                                hide={hiddenNameList.includes(dp.account.name.trim())}
                             />
                         )
                     })}
                 </LineChart>
             </ResponsiveContainer>
-            <MoniToolTip text='Set phone in landscape mode to see all the months!' icon={<LightbulbIcon />} />
+            <MoniToolTip text='You can click on the account name to show / hide the corresponding line!' icon={<LightbulbIcon />} />
         </div>
     );
 }
